f817e1e92789dd70a7ec5eb900be3002e8f3c399
LiZard0309 <lisahofbauer3992@gmail.com>
2024-10-18 00:22:23 +0200

    modified App.vue to include a link to ContactView.vue, added ContactView.vue, Contact.vue and Directions.vue

---
+++ src/components/Contact.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  name: "Contact",
+
+  data(){
+    return {
+      email: "hotel@email.com"
+    }
+  }
+}
+</script>
+
+<template>
+  <h4>Noch Fragen? </h4>
+  <p> Kontaktieren Sie uns: <a href="">{{email}}</a></p>
+
+</template>
+
+<style scoped>
+
+h4 {
+  font-weight: bold;
+}
+
+</style>
---
+++ src/views/ContactView.vue
@@ -0,0 +1,24 @@
+<script>
+import Contact from "@/components/Contact.vue";
+import Directions from "@/components/Directions.vue";
+
+export default {
+  name: "ContactView",
+  components: {Directions, Contact}
+}
+</script>
+
+<template>
+  <body>
+  <Directions></Directions>
+  <Contact></Contact>
+  </body>
+
+
+</template>
+
+<style scoped>
+body {
+  margin: 2rem;
+}
+</style>
