bcfae89f54cd24cedd70b70fde782bbdd77a6997
Maria Feher-Lehrner <maria@feher.at>
2024-10-18 11:34:11 +0200

    * Added a card component for room presentation.
    * Added placeholder image to assets
    
    * Added king size double bed icon to assets
    
    * Added Routing to RoomsView page to App.vue and index.js

---
+++ src/components/rooms-card.vue
@@ -0,0 +1,42 @@
+<script>
+import IconBedKingOutline from "@/components/icons/IconBedKingOutline.vue";
+import bedroomImage from "@/assets/bedroom.jpg";
+
+export default {
+  name: "rooms-card",
+  components: {IconBedKingOutline},
+  data() {
+    return {
+      bedroomImage,
+    };
+  },
+}
+</script>
+
+<template>
+  <div>
+    <b-card
+        title="Zimmer Name"
+        :img-src="bedroomImage"
+        img-alt="Image"
+        img-top
+        tag="article"
+        style="max-width: 20rem;"
+        class="mb-2"
+    >
+      <b-card-text>
+        Preis €/Nacht
+      </b-card-text>
+
+      <b-button href="#" variant="primary">Verfügbarkeit prüfen</b-button>
+      <br/>
+      <IconBedKingOutline></IconBedKingOutline>
+      <p>2 Betten</p>
+
+    </b-card>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- src/views/RoomsView.vue
+++ src/views/RoomsView.vue
@@ -1,11 +1,14 @@
 <script>
+import RoomsCard from "@/components/rooms-card.vue";
+
 export default {
-  name: "RoomsView"
+  name: "RoomsView",
+  components: {RoomsCard}
 }
 </script>
 
 <template>
-
+<rooms-card></rooms-card>
 </template>
 
 <style scoped>
