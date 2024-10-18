8b2c6b86984217fbe8d6c1872b7f5599b6c985a0
Paul Kogler <paulkogler@MacBook-Pro-von-Paul.local>
2024-10-18 14:09:18 +0200

    clean up

---
+++ src/components/ImageSlider.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+  <h1>Willkommen in unserem Hotel!</h1>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, in, quaerat. Aliquid beatae debitis deleniti distinctio dolores, earum est facilis iure nisi, obcaecati pariatur repellat repudiandae similique tempora velit voluptas!</p>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, in, quaerat. Aliquid beatae debitis deleniti distinctio dolores, earum est facilis iure nisi, obcaecati pariatur repellat repudiandae similique tempora velit voluptas!</p>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, in, quaerat. Aliquid beatae debitis deleniti distinctio dolores, earum est facilis iure nisi, obcaecati pariatur repellat repudiandae similique tempora velit voluptas!</p>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, in, quaerat. Aliquid beatae debitis deleniti distinctio dolores, earum est facilis iure nisi, obcaecati pariatur repellat repudiandae similique tempora velit voluptas!</p>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, in, quaerat. Aliquid beatae debitis deleniti distinctio dolores, earum est facilis iure nisi, obcaecati pariatur repellat repudiandae similique tempora velit voluptas!</p>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, in, quaerat. Aliquid beatae debitis deleniti distinctio dolores, earum est facilis iure nisi, obcaecati pariatur repellat repudiandae similique tempora velit voluptas!</p>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, in, quaerat. Aliquid beatae debitis deleniti distinctio dolores, earum est facilis iure nisi, obcaecati pariatur repellat repudiandae similique tempora velit voluptas!</p>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, in, quaerat. Aliquid beatae debitis deleniti distinctio dolores, earum est facilis iure nisi, obcaecati pariatur repellat repudiandae similique tempora velit voluptas!</p>
+</template>
