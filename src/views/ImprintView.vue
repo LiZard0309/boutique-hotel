ee65e22a2b643776f1547ed3475d6151f7d68969
Maria Feher-Lehrner <maria@feher.at>
2024-10-19 20:24:14 +0200

    * changed dummy data in ImprintView.vue
    * added container-div and padding to Imprint content

--- src/views/ImprintView.vue
+++ src/views/ImprintView.vue
@@ -5,22 +5,39 @@ export default {
 </script>
 
 <template>
-<h1>Impressum</h1>
-  <br/>
-  <p>Webseitenbetreiber: Alpenblick Resort GmbH<br/>
-    Firmenbuchnummer: 123456789<br/>
-    Firmenbuchgericht: Handelsgericht Innsbruck</p>
-  <p>Anschrift: Alpenstraße, 1, 6000 Tirolerstadt</p>
-  <p>UID-Nr: 123456789<br/>
-    Gewerbeaufsichtbehörde: Bezirkshauptmannschafft Innsbruck<br/>
-    Mitgliedschaften: Mitglied der Bundesinnung</p>
-  <br/>
-  <p>Kontaktdaten:<br/>
-    Telefon: 05123456789<br/>
-    Email: office@alpenblick-resort.at</p>
+  <div>
+    <div class="row imprint-container">
+      <div class="col-md-6 mx-auto">
+        <h1>Impressum</h1>
+        <br/>
+        <h2>Angaben gemäß § 5 TMG:</h2>
+        <p>Hotel La Vie Élégante<br/>
+          Inhaberin: Marie Dupont<br/>
+          Bergstraße 12<br/>
+          12345 Sonnental<br/>
+          Deutschland</p>
+        <h2>Kontakt:</h2>
+        <p>Telefon: +49 (0) 123 456789<br/>
+          E-Mail: info@lavieelegante.de<br/>
+          Website: www.lavieelegante.de</p>
+        <p>UID-Nr: DE123456789<br/>
+          Finanzamt Sonnental</p>
+        <br/>
+        <h2>Verantwortlich für den Inhalt nach § 55 Abs. 2 RStV:</h2>
+        <p>Marie Dupont<br/>
+          Bergstraße 12<br/>
+          12345 Sonnental<br/>
+          Deutschland</p>
+      </div>
+    </div>
+  </div>
 
 </template>
 
 <style scoped>
+
+.imprint-container{
+  padding: 0.5rem;
+}
 
 </style>
