97897de12bfb0fdc7b8b19a36536be0c547a394b
Paul Kogler <paulkogler@MacBook-Pro-von-Paul.local>
2024-10-19 12:37:52 +0200

    Add zimmer button

---
+++ src/components/RoomsButton.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  name: "RoomsButton"
+}
+</script>
+
+<template>
+  <div class="container">
+    <b-button squared variant="outline-primary" size="lg" >Unsere Zimmer</b-button>
+  </div>
+</template>
+
+<style scoped>
+.container {
+font-weight: bolder;
+}
+
+</style>
--- src/components/WelcomeHeadline.vue
+++ src/components/WelcomeHeadline.vue
@@ -1,14 +1,22 @@
 <script>
+import RoomsButton from "@/components/RoomsButton.vue";
+
 export default {
-  name: "Welcome"
+  name: "Welcome",
+  components: {RoomsButton}
 }
 </script>
 
 <template>
   <div class="container">
-    <h1>__________</h1>
-    <h1>4 Sterne Superior Hotel La Vie Élégante</h1>
-    <h3> – wo Luxus auf Herzlichkeit trifft –</h3>
+    <div>
+      <h1>__________</h1>
+      <h1>4 Sterne Superior Hotel La Vie Élégante</h1>
+      <h3> – wo Luxus auf Herzlichkeit trifft –</h3>
+    </div>
+    <div>
+      <rooms-button/>
+    </div>
   </div>
 </template>
 
@@ -24,6 +32,11 @@ h3 {
 }
 
 .container {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  align-items: flex-end;
   margin: 10px;
   color: dimgrey;
 }
