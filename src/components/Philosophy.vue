d1d73dac591c6bee781e2c4c24ac4b4cd218e12b
StefanieBaeuchler <stefanie.baeuchler@hotmail.com>
2024-10-19 14:59:02 +0200

    adapted content of Philosophy component and changed layout and style of the AboutVie.vue page

--- src/components/Philosophy.vue
+++ src/components/Philosophy.vue
@@ -3,12 +3,77 @@
 </script>
 
 <template>
-<h1>Gastfreundschaft am Berg</h1>
+
+  <div class="container">
+
+    <h1>Gastfreundschaft am Berg</h1>
+
+    <br>
+    <br>
+
+    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+      dolore
+      magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd
+      gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
+      sadipscing
+      elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero
+      eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+      ipsum
+      dolor sit amet. </p>
+
+    <br>
+
+    <h3>Unsere Philosophie</h3>
+
+    <br>
+
+    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+      dolore
+      magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd
+      gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
+      sadipscing
+      elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero
+      eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+      ipsum
+      dolor sit amet. </p>
+
+    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+      dolore
+      magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd
+      gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
+      sadipscing
+      elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero
+      eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+      ipsum
+      dolor sit amet. </p>
+
+    <br>
+
+    <h5>Wir freuen uns auf Ihren Besuch,</h5>
+    <div class="hostname">Ihre Familie Berger</div>
+
+    <br>
+
+  </div>
+
 </template>
 
 <style scoped>
-h1 {
-  text-align: center;
+
+.hostname {
+  font-family: "Caveat";
+  font-size: 35px
+}
+
+p {
+  text-align: justify;
+}
+
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 80%;
 }
 
 </style>
