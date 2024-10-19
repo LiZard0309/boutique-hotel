e6e643e83910528ec921654781dee8b0bf2b8a2b
Paul Kogler <paulkogler@MacBook-Pro-von-Paul.local>
2024-10-19 11:06:56 +0200

    Add imageslider Komponente

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,21 @@ import Navbar from "@/components/Navbar.vue";
 </script>
 
 <template>
-  <navbar></navbar>
-  <home-view></home-view>
+  <div id="app">
+    <div class="main-content">
+      <navbar></navbar>
+      <home-view></home-view>
+    </div>
+  </div>
 
 </template>
 
 <style scoped>
+
+.main-content {
+  max-width: 1200px; /* Maximale Breite der App */
+  margin: 0 auto;
+  padding: 20px 20px;
+}
 
 </style>
