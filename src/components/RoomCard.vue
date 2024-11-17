026d0bd2866387a7a6afa2c8a01c02bf77bd7867
Maria Feher-Lehrner <maria@feher.at>
2024-11-17 18:52:14 +0100

    bugfix: added another availability check for room card with refresh for selected date range before start of booking process on button click "Reservieren". -->

--- src/components/RoomCard.vue
+++ src/components/RoomCard.vue
@@ -1,6 +1,6 @@
 <script>
 import IconBedKingOutline from "@/components/icons/IconBedKingOutline.vue";
-import { BCard, BButton, BCollapse, BModal } from "bootstrap-vue-3";
+import {BCard, BButton, BCollapse, BModal} from "bootstrap-vue-3";
 import RoomActions from "./roomCard/RoomActions.vue";
 import RoomDetails from "./roomCard/RoomDetails.vue";
 import RoomAvailabilityInfo from "./roomCard/RoomAvailabilityInfo.vue";
@@ -13,7 +13,12 @@ export default {
     RoomDetails,
     RoomActions,
     RoomAvailabilityInfo,
-    IconBedKingOutline, BCard, BButton, BCollapse, BModal, DateRangePicker
+    IconBedKingOutline,
+    BCard,
+    BButton,
+    BCollapse,
+    BModal,
+    DateRangePicker
   },
   props: {
     roomId: {type: Number, required: true,},
@@ -22,24 +27,35 @@ export default {
     image: {type: String, required: true,},
     beds: {type: Number, required: true,},
     extras: {type: Array, required: true,},
+    //selectedDates: {type: String, required: false, default: null},
   },
   methods: {
     displayDatePickerModal() {
       this.showModal = true;
     },
-    reserveRoom () {
-      console.log("Zimmer wird reserviert:", this.roomId, this.selectedDateRange);
+    async reserveRoom() {
+      await this.submitDates();
+      const response = this.roomsStore.apiData;
+      console.log("current Date in roomsStore:", this.roomsStore.dateRange);
+      console.log("current Date in this.selectedDates:", this.selectedDates);
+      if (response === true) {
+        console.log("Zimmer wird reserviert:", this.roomId, this.selectedDateRange)
+      }
     },
     handleDateSelection(dates) {
-      this.selectedDates = dates; // Store selected dates if needed
+      this.selectedDates = dates;
     },
-    async submitDates() {
+    async startDateCheck() {
       this.selectedDateRange = `${this.selectedDates.start} - ${this.selectedDates.end}`;
 
-      this.roomsStore.setDateRange(this.selectedDates.start, this.selectedDates.end);
+      /*this.roomsStore.setDateRange(this.selectedDates.start, this.selectedDates.end);
       await this.roomsStore.fetchRoomAvailability(this.roomId);
       const response = this.roomsStore.apiData
-      console.log("current apiData:", this.roomsStore.apiData);
+      console.log("current apiData:", this.roomsStore.apiData);*/
+
+      await this.submitDates();
+      const response = this.roomsStore.apiData;
+      console.log("response after refactor:", response);
 
       if (response === true) {
         this.isAvailable = true;
@@ -49,7 +65,13 @@ export default {
 
       this.availabilityChecked = true;
       this.showModal = false; // Close the modal after submission
+    },
+
+    async submitDates() {
+      this.roomsStore.setDateRange(this.selectedDates.start, this.selectedDates.end);
+      await this.roomsStore.fetchRoomAvailability(this.roomId);
     }
+
   },
   data() {
     return {
@@ -111,7 +133,7 @@ export default {
           />
         </b-card>
 
-        <b-modal v-model="showModal" title="Wählen Sie Ihre Reisedaten" @ok="submitDates">
+        <b-modal v-model="showModal" title="Wählen Sie Ihre Reisedaten" @ok="startDateCheck">
           <DateRangePicker @date-selected="handleDateSelection"/>
         </b-modal>
 
@@ -131,6 +153,7 @@ export default {
   justify-content: space-between;
   align-items: center;
 }
+
 .left-content p {
   font-size: 1.2rem;
   margin-left: 10px;
