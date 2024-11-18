6e1c9d245d1a3896a9994d44c9eb2abca1a75fbd
Maria Feher-Lehrner <maria@feher.at>
2024-11-18 10:00:07 +0100

    bugfix: fixed typo in alertMessages and added styling for width of alertBox

--- src/components/modals/alertMessages.js
+++ src/components/modals/alertMessages.js
@@ -1,4 +1,4 @@
 export default {
     success: "Buchung erfolgreich! Ihre Buchungs-ID ist",
-    error: "Die Buchung ist leider fehlgeschlagen. Möglicherweise wurde das Zimmer in der Zwischenzeit bereits gebucht. Klicken Sie auf Ok, um zurück zur Zimmerübersicht zu kommen.",
+    error: "Die Buchung ist leider fehlgeschlagen. Möglicherweise wurde das Zimmer in der Zwischenzeit bereits gebucht. Klicken Sie auf OK, um zurück zur Zimmerübersicht zu kommen.",
 };
