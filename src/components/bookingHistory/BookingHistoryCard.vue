a03f06a6eafd2e29f36d4322ad9ba6091f683516
Maria Feher-Lehrner <maria@feher.at>
2024-12-14 17:27:44 +0100

    feature: started preparing page BookingHistoryView.vue and component BookingHistoryCard.vue

---
+++ src/components/bookingHistory/BookingHistoryCard.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  name: "BookingHistoryCard",
+  components: {
+
+  }
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
--- src/views/BookingHistoryView.vue
+++ src/views/BookingHistoryView.vue
@@ -1,11 +1,37 @@
 <script>
+import BookingHistoryCard from "@/components/bookingHistory/BookingHistoryCard.vue";
+
 export default {
-  name: "BookingHistoryView"
+  name: "BookingHistoryView",
+  components: {BookingHistoryCard}
 }
 </script>
 
 <template>
+  <h1>Buchungshistorie</h1>
+  <h2>Bevorstehende Buchungen</h2>
+  <div class="booking-history-container">
+    <div v-if="isLoading">Lade Buchungen...</div>
+    <div v-else>
+      <div v-for="(booking) in bookings" :key="booking.id">
 
+      </div>
+    </div>
+  </div>
+  <div class="booking-history-container">
+  </div>
+
+  <h2>Vergangene Buchungen</h2>
+  <div class="booking-history-container">
+    <div v-if="isLoading">Lade Buchungen...</div>
+    <div v-else>
+      <div v-for="(booking) in bookings" :key="booking.id">
+
+      </div>
+    </div>
+  </div>
+  <div class="booking-history-container">
+  </div>
 </template>
 
 <style scoped>
