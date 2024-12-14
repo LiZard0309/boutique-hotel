35354a57acd60dfdbabdec25745d0bce942c20bc
Paul Kogler <paulkogler@MacBook-Pro-von-Paul.local>
2024-12-14 18:07:50 +0100

    add new Confirmation Component

--- src/views/BookingConfirmationView.vue
+++ src/views/BookingConfirmationView.vue
@@ -1,10 +1,12 @@
 <script setup>
 
+import ConfirmationNotification from "@/components/booking/ConfirmationNotification.vue";
 </script>
 
 <template>
 <h1> Booking Confirmation View</h1>
   <p>Hier kommen alle Komponenten für die Bestätigung</p>
+  <ConfirmationNotification message="Messsage" header="Header" is-successful="true" />
 </template>
 
 <style scoped>
