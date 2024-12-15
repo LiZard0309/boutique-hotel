b023211e4943ab5d7d86e961b340f4d128a7b21a
Maria Feher-Lehrner <maria@feher.at>
2024-12-15 18:27:15 +0100

    feature: added pagination logic to Bookings display

--- src/views/BookingHistoryView.vue
+++ src/views/BookingHistoryView.vue
@@ -1,15 +1,46 @@
 <script>
-import BookingHistoryCard from "@/components/bookingHistory/BookingHistoryCard.vue";
+import BookingHistoryCard from "@/components/booking/BookingHistoryCard.vue";
+import {useUserStore} from "@/stores/user";
+import {usePaginationStore} from "../stores/usePaginationStore";
+import BookingsPagination from "../components/booking/BookingsPagination.vue";
+import RoomPagination from "../components/RoomPagination.vue";
 
 export default {
   name: "BookingHistoryView",
-  components: {BookingHistoryCard}
+  components: {RoomPagination, BookingHistoryCard, BookingsPagination},
+
+  data() {
+    return {
+      perPage: usePaginationStore().perPage,
+      isLoading: true,
+    };
+  },
+
+  computed: {
+    userStore() {
+      return useUserStore();
+    },
+    bookings() {
+      return this.userStore.bookings;
+    },
+    currentPage() {
+      return usePaginationStore().currentPage
+    },
+    paginatedBookings() {
+      const start = (this.currentPage - 1) * this.perPage;
+      return this.bookings.slice(start, start + this.perPage);
+    }
+  },
+
+  methods: {
+
+  }
 }
 </script>
 
 <template>
   <h1>Buchungshistorie</h1>
-  <h2>Bevorstehende Buchungen</h2>
+
   <div class="booking-history-container">
     <div v-if="isLoading">Lade Buchungen...</div>
     <div v-else>
@@ -17,23 +48,13 @@ export default {
 
       </div>
     </div>
-  </div>
-  <div class="booking-history-container">
-  </div>
 
-  <h2>Vergangene Buchungen</h2>
-  <div class="booking-history-container">
-    <div v-if="isLoading">Lade Buchungen...</div>
-    <div v-else>
-      <div v-for="(booking) in bookings" :key="booking.id">
-
-      </div>
-    </div>
-  </div>
-  <div class="booking-history-container">
+    <room-pagination :rows="bookings.length"></room-pagination>
   </div>
 </template>
 
 <style scoped>
-
+.booking-history-container {
+  padding: 0.5rem;
+}
 </style>
