735d4c57e0623552152cc70026d3ba6661427765
Maria Feher-Lehrner <maria@feher.at>
2024-12-15 16:40:56 +0100

    feature: added pagination for bookings history display

---
+++ src/components/booking/BookingsPagination.vue
@@ -0,0 +1,43 @@
+<script>
+import {usePaginationStore} from "../../stores/usePaginationStore";
+import {useUserStore} from "../../stores/user";
+
+export default {
+  name: "BookingsPagination",
+  methods: {useUserStore},
+  props: {
+    rows: Number
+  },
+  data: () => {
+    return {
+      currentPage: 1
+    }
+  },
+  computed: {
+    perPage() {
+      return usePaginationStore().perPage
+    },
+  },
+  watch: {
+    currentPage(latest) {
+      usePaginationStore().setCurrentPage(latest);
+    }
+  }
+}
+</script>
+
+<template>
+  <b-pagination
+      v-model="currentPage"
+      :total-rows="rows"
+      :per-page="perPage"
+      aria-controls="bookings-cards"
+      align="center"
+      class="mt-3"
+  ></b-pagination>
+
+</template>
+
+<style scoped>
+
+</style>
