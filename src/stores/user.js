0995e5dafd5eb6a1d6bb20ef795b57a82bd260a6
Maria Feher-Lehrner <maria@feher.at>
2024-12-17 15:03:58 +0100

    feature: implemented axios call for booking history in store

--- src/stores/user.js
+++ src/stores/user.js
@@ -1,6 +1,27 @@
 import {defineStore} from 'pinia'
 import axios from 'axios'
 
-export const useUserStore = defineStore('user', {
+const apiUrl = "https://boutique-hotel.helmuth-lammer.at/api/v1/"
 
+export const useUserStore = defineStore('user', {
+    state: () => ({
+        bookings: [],
+        token: "",
+    }),
+
+    actions: {
+        async fetchBookingHistory(token) {
+            try {
+                const response = await axios.get('${apiUrl}/user/bookings', {
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    }
+                })
+                this.bookings = response.data;
+                console.log(this.bookings);
+            } catch (error) {
+                console.error("Error fetching booking history:", error);
+            }
+        }
+    }
 })
