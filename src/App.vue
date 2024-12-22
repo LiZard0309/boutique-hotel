9e89d530c065a47214de95fa8ead0ac286d76a22
Maria Feher-Lehrner <maria@feher.at>
2024-12-22 12:23:50 +0100

    feature: added spinner and loading logic to indicate (re-)fetch of data when checking a new room availability date after first one is unavailable.

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <script setup>
 import {RouterLink, RouterView} from 'vue-router'
 import Footer from '@/global/Footer.vue'
-
-
+import {useRoomsStore} from "@/stores/rooms";
 import HomeView from "@/views/HomeView.vue";
 import Navbar from "@/global/Navbar.vue";
+import Spinner from "@/global/Spinner.vue";
+
+const roomsStore = useRoomsStore();
 </script>
 
 <template>
   <navbar></navbar>
   <div id="app">
     <div class="main-content">
+      <Spinner v-if="roomsStore.loading" />
       <RouterView/>
     </div>
 
---
+++ src/global/Spinner.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  name: "Spinner"
+}
+</script>
+
+<template>
+  <div class="spinner">
+    <div class="spinner-border" role="status">
+      <span class="visually-hidden">Loading...</span>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.spinner {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100px;
+}
+</style>
